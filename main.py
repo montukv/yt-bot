468c95124633294aa8fd02357662dda389264f93
montukv <46618399+montukv@users.noreply.github.com>
2020-11-07 18:51:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ videos = ['https://www.youtube.com/watch?v=Bp2Ewg-k0_A',
 sleep_time = 0
 
 for i in range(1000):
-	random_video = random.randint(0,4)
+	random_video = random.randint(0,3)
 	driver.get(videos[random_video])
 	time.sleep(sleep_time) # Let the user actually see something!
-	sleep_time = random.randint(100,200)
+	sleep_time = random.randint(10,20)
 
 	time.sleep(5) # Let the user actually see something!
 driver.quit()
